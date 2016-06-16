66780b61e67cc2c03fff7db43b5f906315e07a81
Laju Morrison <morrelinko@gmail.com>
2016-06-17 00:11:34 +0100

    Ensure database identifier can only be used once...

--- generators/database/steps/prompting.js
+++ generators/database/steps/prompting.js
@@ -1,13 +1,23 @@
 'use strict';
 
+let chalk = require('chalk');
 let questions = require('../questions/main');
 
 module.exports = function (program) {
   return function () {
+    let config = program.config(this);
     let promise = this.prompt(questions(program, this));
 
     promise = promise.then(function (answers) {
       this.answers = Object.assign(this.answers || {}, answers);
+
+      if (this.answers.identifier in config.get('database')) {
+        this.log(chalk.red(
+          `Database with identifier ${chalk.cyan(
+            this.answers.identifier)} already in use.`
+        ));
+        process.exit(1);
+      }
     }.bind(this));
 
     return promise;
